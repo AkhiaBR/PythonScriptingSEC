5e9b9ab0c24be06735367bd1972266dae7d4798d
Fernando <fernandooogoncalves@gmail.com>
2025-07-20 15:32:31 -0300

    portScanner.py finalizado

---
+++ Ferramentas/portScanner.py
@@ -0,0 +1,19 @@
+import socket, sys
+
+if (len(sys.argv) < 2):
+    print("ERRO: Argumentos insuficientes.")
+    print("EXEMPLO DE USO: python script.py <IP> <porta>")
+    sys.exit(1)
+else:
+    ip = str(sys.argv[1])
+
+    for porta in range(1, 65536):
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conexao = mySocket.connect_ex((ip, porta))
+
+        if (conexao == 0):
+            print(f"Porta {porta} aberta no host {ip}")
+
+        mySocket.close()
+
+    sys.exit(0)
---
+++ Introducao/touchLoop.py
@@ -0,0 +1,7 @@
+import os
+
+i = 0
+
+while True:
+    i+=1
+    os.system(f"touch trolled.{i}.txt")
